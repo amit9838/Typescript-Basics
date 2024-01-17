8b4f449d2f5443f9e64d0c4490d62dec54b15315
Amit Chaudhary <amitchaudhary0539@outlook.com>
2024-01-18 01:13:50 +0530

    lecture-3

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,21 @@
-var character = 'mario';
-console.log(character);
-var inputs = document.querySelectorAll('input');
-inputs.forEach(function (input) {
-    console.log(input);
-});
+var myname = 'mario';
+var age = 30;
+var isBoy = true;
+// myname = 20  // Invalid operation 
+// myname can take string velues only, because it has been decleared with string.
+myname = "Maria"; // Valid operation
+age = 40;
+isBoy = false;
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ("sd"));
+// Here ts is not throwing error even though we are passing string in the function 
+// because function is not asking for any specific datatype as the argument
+// But if we see console is says NaN (not a number), it can be fixed or catched at compile 
+// if we add data type in the finction argument as below
+var circ2 = function (diameter) {
+    return diameter * Math.PI;
+};
+// console.log(circ2("sd"))  // Now this line throws error as invalid arg being passed
+console.log(circ2(5));
--- sandbox.tsx
+++ sandbox.tsx
@@ -1,11 +1,30 @@
-const character = 'mario';
-
-console.log(character);
-
-const inputs = document.querySelectorAll('input');
-
-inputs.forEach(input => {
-  console.log(input);
-});
+let myname = 'mario'
+let age = 30
+let isBoy = true
 
 
+// myname = 20  // Invalid operation 
+// myname can take string velues only, because it has been decleared with string.
+
+myname = "Maria"  // Valid operation
+
+age = 40
+
+isBoy = false
+
+const circ = (diameter) => {
+    return diameter * Math.PI;
+}
+
+console.log(circ("sd")) 
+// Here ts is not throwing error even though we are passing string in the function 
+// because function is not asking for any specific datatype as the argument
+// But if we see console is says NaN (not a number), it can be fixed or catched at compile 
+// if we add data type in the finction argument as below
+
+const circ2 = (diameter:number) => {
+    return diameter * Math.PI;
+}
+
+// console.log(circ2("sd"))  // Now this line throws error as invalid arg being passed
+console.log(circ2(5))
