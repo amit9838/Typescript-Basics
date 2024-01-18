63612b0bc4a9b09668acdf6e9f9aa52bcbc4886a
Amit Chaudhary <amitchaudhary0539@outlook.com>
2024-01-19 01:18:05 +0530

    lecture-6

--- sandbox.js
+++ sandbox.js
@@ -1,43 +1,20 @@
-// Lectute 5 - Explict Types
-// Explicit Types
-var myname;
-// This means 'myname' will only accept strings such as "hello"
-// similarly we can do this for other data types as well
-var age;
-var isLoggedIn;
-// Now, we can to change any of the variable , w'll have to use data type in which it has been initialised like- 
-age = 40;
-// age = "ten"  // Invalid operation : age only takes number data-type
-// ARRAYS
-var ninjas;
-// This means that 'ninjas' array will only take 'string' as array elements
-// We can also initialize with some default value, so that we can directly perform operations like .push()
-// ninjas.push('dsf') // Invalid operation : Variable 'ninjs' is used before initialzed
-var names = [];
-names.push("ion");
-// UNION TYPES
-// It can accept values with multiple data types 
+// Lectute 6 - Dynamic(any) Types
+// 'any' type can accept values of any data-type
+var age = 25;
+age = true; //  it can be boolean
+console.log(age);
+age = "hello"; // It can be string
+console.log(age);
+age = { 'value': 25 }; // It can be object
+console.log(age);
+// Array can accept value of any data-type
 var mixed = [];
-// 'mixed' can accept values from any of the datatype above(string,number,boolean)
-mixed.push("two");
-mixed.push(2);
-mixed.push(false);
-console.log(mixed);
-// Application
-var uid;
-uid = "123";
-uid = 123;
-// OBJECTS
-var person;
-person = { name: "john", age: 23, isAboveEighteen: true };
-var person2;
-person2 = {
-    name: 'josh',
-    age: 'six', //Age as string
-    isAboveEighteen: false
-};
-person2 = {
-    name: 'josh',
-    age: 6, // age as number
-    isAboveEighteen: false
-};
+mixed.push('twelve');
+mixed.push(12);
+mixed.push(true);
+mixed.push({ value: "twelve" });
+// Objects can also have this (any) field
+var ninja;
+ninja = { name: 'jack', age: '22' };
+ninja = { name: 'jack', age: 22 };
+console.log(ninja);
--- sandbox.tsx
+++ sandbox.tsx
@@ -1,64 +1,28 @@
-// Lectute 5 - Explict Types
+// Lectute 6 - Dynamic(any) Types
+// 'any' type can accept values of any data-type
 
-// Explicit Types
-let myname : string
-// This means 'myname' will only accept strings such as "hello"
-// similarly we can do this for other data types as well
+let age: any = 25
 
-let age : number
-let isLoggedIn : boolean
+age = true  //  it can be boolean
+console.log(age)
 
-// Now, we can to change any of the variable , w'll have to use data type in which it has been initialised like- 
-age = 40
+age = "hello" // It can be string
+console.log(age)
 
-// age = "ten"  // Invalid operation : age only takes number data-type
+age = { 'value': 25 } // It can be object
+console.log(age)
 
+// Array can accept value of any data-type
+let mixed: any[] = []
+mixed.push('twelve')
+mixed.push(12)
+mixed.push(true)
+mixed.push({ value: "twelve" })
 
-// ARRAYS
-let ninjas : string[]; 
-// This means that 'ninjas' array will only take 'string' as array elements
-// We can also initialize with some default value, so that we can directly perform operations like .push()
+// Objects can also have this (any) field
+let ninja: { name: any, age: any }
 
-// ninjas.push('dsf') // Invalid operation : Variable 'ninjs' is used before initialzed
+ninja = { name: 'jack', age: '22' }
+ninja = { name: 'jack', age: 22 }
 
-let names : string[] = []
-names.push("ion")
-
-// UNION TYPES
-// It can accept values with multiple data types 
-
-let mixed : (string|number|boolean)[] = []
-// 'mixed' can accept values from any of the datatype above(string,number,boolean)
-mixed.push("two")
-mixed.push(2)
-mixed.push(false)
-
-console.log(mixed)
-
-
-// Application
-let uid : string|number
-uid = "123";
-uid = 123;
-
-// OBJECTS
-let person : object;
-person = {name:"john", age:23, isAboveEighteen:true}
-
-let person2 : {
-    name : string,
-    age : number|string
-    isAboveEighteen : boolean
-}
-
-person2 = {
-    name:'josh',
-    age:'six',  //Age as string
-    isAboveEighteen:false
-}
-
-person2 = {
-    name:'josh',
-    age:6,   // age as number
-    isAboveEighteen:false
-}
+console.log(ninja)
